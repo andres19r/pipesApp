175e5186c5d4031477c349941c7bac4ea39fcddc
Andres Rivero <andres19rivero@gmail.com>
2022-07-11 19:42:10 -0400

    sales module & added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRouterModule } from './app-router.module';
 
 import { SharedModule } from './shared/shared.module';
 
@@ -10,6 +11,7 @@ import { SharedModule } from './shared/shared.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AppRouterModule,
     SharedModule
   ],
   providers: [],
