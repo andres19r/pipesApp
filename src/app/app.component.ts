4a89fe71a8594a53ef5ff8fc6b84adbdc80e4406
Andres Rivero <andres19rivero@gmail.com>
2022-07-10 22:10:46 -0400

    added global style theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  name = 'andres riVeRo';
+
+  showName() {
+    console.log(this.name);
+  }
 }
