5493e0c7db5a99e0b3b31a7a3b5cfd0d79ed9249
Andres Rivero <andres19rivero@gmail.com>
2022-07-11 21:14:08 -0400

    added routes to the menu bar

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -14,25 +14,30 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-        label: 'File',
+        label: 'Angular Pipes',
+        icon: 'pi pi-desktop',
         items: [
           {
-            label: 'New',
-            icon: 'pi pi-fw pi-plus',
-            items: [{ label: 'Project' }, { label: 'Other' }],
+            label: 'Text & Dates',
+            icon: 'pi pi-align-left',
+            routerLink: '/'
           },
-          { label: 'Open' },
-          { label: 'Quit' },
-        ],
+          {
+            label: 'Numbers',
+            icon: 'pi pi-dollar',
+            routerLink: '/numbers'
+          },
+          {
+            label: 'No commons',
+            icon: 'pi pi-globe',
+            routerLink: '/no-commons'
+          },
+        ]
       },
       {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' },
-        ],
-      },
+        label: 'Custom Pipes',
+        icon: 'pi pi-cog'
+      }
     ];
   }
 }
