bc4ffd3f5ef1045934a428bf25c2c2df83eddd42
Andres Rivero <andres19rivero@gmail.com>
2022-07-14 20:07:38 -0400

    currency & percent pipe

--- src/app/sales/pages/numbers/numbers.component.ts
+++ src/app/sales/pages/numbers/numbers.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NumbersComponent implements OnInit {
   netSales: number = 2567789.5567
-  percentage: number = 0.48
+  percentage: number = 0.4856
 
   constructor() { }
 
