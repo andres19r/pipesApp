045567dbe29818389cd15572e08864f3d18c8a65
Andres Rivero <andres19rivero@gmail.com>
2022-07-14 22:07:35 -0400

    i18nplural pipe

--- src/app/sales/pages/no-commons/no-commons.component.ts
+++ src/app/sales/pages/no-commons/no-commons.component.ts
@@ -6,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./no-commons.component.css']
 })
 export class NoCommonsComponent implements OnInit {
+  // i18nSelect
   name: string = 'Andrés'
   gender: string = 'male'
   invitationMap = {
     'male': 'invitarlo',
     'female': 'invitarla',
+  }
+
+  // i18nPlural
+  clients: string[] = ['Maria', 'Pedro', 'Juan']
+  clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos dos clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
   }
 
   constructor() { }
