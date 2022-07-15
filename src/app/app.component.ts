4126eadd1065c5ed133f0322b78eda436d82dc92
Andres Rivero <andres19rivero@gmail.com>
2022-07-14 20:26:45 -0400

    prepare the fieldsets for the next pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'andres riVeRo';
+export class AppComponent implements OnInit {
+  constructor(private primeNgConfig: PrimeNGConfig) {}
 
-  showName() {
-    console.log(this.name);
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { AppRouterModule } from './app-router.module';
@@ -18,6 +19,7 @@ registerLocaleData(localeFr);
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRouterModule,
     SharedModule,
     SalesModule
