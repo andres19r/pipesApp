a5b0604edae278257c1b321a121472d0dbbb78d5
Andres Rivero <andres19rivero@gmail.com>
2022-07-16 12:15:29 -0400

    filled the table

--- src/app/sales/pages/sort/sort.component.ts
+++ src/app/sales/pages/sort/sort.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Hero } from '../../interfaces/sales.interface';
 
 @Component({
   selector: 'app-sort',
   templateUrl: './sort.component.html',
-  styleUrls: ['./sort.component.css']
+  styleUrls: ['./sort.component.css'],
 })
 export class SortComponent implements OnInit {
-  inUpper: boolean = true
+  inUpper: boolean = true;
+  heroes: Hero[] = [
+    { name: 'Superman', fly: true, color: Color.blue },
+    { name: 'Batman', fly: false, color: Color.black },
+    { name: 'Robin', fly: false, color: Color.green },
+    { name: 'Daredevil', fly: false, color: Color.red },
+    { name: 'Green Lantern', fly: true, color: Color.green },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   toggleUpper(): void {
-    this.inUpper = !this.inUpper
+    this.inUpper = !this.inUpper;
   }
 }
